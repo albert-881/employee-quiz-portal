594b2315ce8d08cd6f499060054dd1e983a3b622
albert-881 <quinteroalberto88@gmail.com>
2025-02-19 14:03:51 -0600

    added a admin page and added demo functionality if the system sees admin credentials it redirects them to the admin page where they will be able to create a quiz name and description for now more functionality will come later

--- js/validation.js
+++ js/validation.js
@@ -3,6 +3,9 @@ import { errorMSG } from "./ui.js";
 const validEmail = 'quinteroalberto88@gmail.com';
 const validPassword = '1234'; 
 
+const validAdminEmail = 'admin@123.com';
+const validAdminPassword = '4321';
+
 export function test() {
   let email = document.querySelector('#email').value;
   let password = document.querySelector('#password').value;
@@ -16,7 +19,10 @@ export function verify(e, p) {
   console.log('verify function works');
   if (e === validEmail && p === validPassword) {
     window.location.href = 'quiz-list.html';
-  } 
+  }
+  else if (e === validAdminEmail && p === validAdminPassword){
+    window.location.href = 'admin.html';
+  }
   else {
     errorMSG();
   }
