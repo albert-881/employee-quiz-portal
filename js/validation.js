2c2ef8998736788eb6eaae83340344085cca2070
albert-881 <quinteroalberto88@gmail.com>
2025-02-19 11:18:22 -0600

    improved error handling for login verification. added a message when the user inputs incorrect credentials

--- js/validation.js
+++ js/validation.js
@@ -1,18 +1,27 @@
+const loginVerification = document.querySelector('#login-form');
+const errorMSG = document.createElement('p');
+
 const validEmail = 'quinteroalberto88@gmail.com';
 const validPassword = '1234';
 
 export function test() {
-    let email = document.querySelector('#username').value;
-    let password = document.querySelector('#password').value;
-    console.log('test function works');
-    verify(email, password);
+  let email = document.querySelector('#email').value;
+  let password = document.querySelector('#password').value;
+  console.log('test function works');
+  verify(email, password);
 }
 
 export function verify(e, p) {
-    console.log('verify function works');
-    if (e === validEmail && p === validPassword) {
-        window.location.href = 'quiz-list.html';
-    } else {
-        alert('Invalid email or password!');
-    }
+  
+    
+  console.log('verify function works');
+  if (e === validEmail && p === validPassword) {
+    window.location.href = 'quiz-list.html';
+  } 
+  else {
+    errorMSG.innerHTML = '';
+    errorMSG.innerHTML = 'Invalid Email or Password';
+    loginVerification.appendChild(errorMSG);
+    
+  }
 }
